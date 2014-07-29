6477d13c6b1092fa3d42c56b3c3465f86c5bda00
ivan-branets <ivan.branets@gmail.com>
2014-07-29 22:33:16 +0300

    print reversed llinked list

---
+++ Mct_67/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DataStructures.Lists;
+using Extensions;
+
+namespace Mct_67
+{
+    class Program
+    {
+        static void Main()
+        {
+            var list = new LinkedList<int> { 1, 2, 3, 4 };
+            PrintReversed(list);
+            Console.ReadKey();
+        }
+
+        static void PrintReversed(LinkedList<int> list)
+        {
+            list.Reverse();
+            list.Log();
+            list.Reverse();
+        }
+    }
+}
