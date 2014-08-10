feb614181d1bb2185ec4d325123596fa937377a9
ivan-branets <ivan.branets@gmail.com>
2014-08-10 18:58:31 +0300

    added enumeration of sequence with equal ones in binary

--- Mct_73/Program.cs
+++ Mct_73/Program.cs
@@ -6,80 +6,28 @@ namespace Mct_73
     {
         static void Main()
         {
-            Console.WriteLine("{0}\t{1}", 12, F(12));
-
             for (uint k = 0; k <= 16; k++)
             {
-                Console.WriteLine("{0}\t{1}", k, F(k));
+                Console.WriteLine("f({0}) = {1}", k, F(k));
             }
+
+            Console.WriteLine("f({0}) = {1}", 1024, F(1024));
+            Console.WriteLine("f({0}) = {1}", uint.MaxValue, F(uint.MaxValue));
 
             Console.ReadKey();
         }
 
-        static int F(uint k)
+        private static int F(uint k)
         {
-            int mostSignificantBitIndex;
+            var enumerator = new SequenceWithEqualOnes(k).GetEnumerator();
+            var index = 1;
 
-            uint original = GetFirstNumberWithEquelOnes(k, out mostSignificantBitIndex);
-            int originalMostSignificantBitIndex = mostSignificantBitIndex;
-            uint n = original;
-            int steps = 1;
-
-            while (n != k)
+            while (enumerator.MoveNext() && enumerator.Current != k)
             {
-                if (originalMostSignificantBitIndex == mostSignificantBitIndex)
-                {
-                    mostSignificantBitIndex++;
-                    continue;
-                }
-
-                n = original;
-
-                var mask = ~((uint)1 << (originalMostSignificantBitIndex - 1));
-                n &= mask;
-
-                mask = (uint)1 << (mostSignificantBitIndex - 1);
-                n |= mask;
-
-                var next = original << (steps - 1);
-
-                for (var i = mostSignificantBitIndex - 1; n != next && n != k; i--)
-                {
-                    var prev = n;
-
-                    mask = ~((uint)1 << i - 1);
-                    n &= mask;
-
-                    if (prev == n) continue;
-
-                    mask = (uint)1 << i;
-                    n |= mask;
-
-                    steps++;                    
-                }
-
-                mostSignificantBitIndex++;
+                index++;
             }
 
-            return steps;
-        }
-
-        static uint GetFirstNumberWithEquelOnes(uint k, out int mostSignificantBitIndex)
-        {
-            uint firstNumber = 0;
-            mostSignificantBitIndex = 0;
-
-            for (uint i = k; i > 0; i >>= 1)
-            {
-                if ((i & 1) == 1)
-                {
-                    firstNumber <<= 1;
-                    firstNumber++;
-
-                    mostSignificantBitIndex++;
-                }
-            }
-            return firstNumber;
+            return index;
         }
     }
 }
---
+++ Mct_73/SequenceWithEqualOnes.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Mct_73
+{
+    public class SequenceWithEqualOnes : IEnumerable<uint>
+    {
+        public SequenceWithEqualOnes(uint number)
+        {
+            CurrentNumber = GetFirstNumberWithEquelOnes(number, out _oneCount);
+        }
+
+        private uint CurrentNumber { get; set; }
+        private readonly int _oneCount;
+
+        public IEnumerator<uint> GetEnumerator()
+        {
+            while (CurrentNumber < uint.MaxValue)
+            {
+                yield return CurrentNumber;
+
+                var newNumber = CurrentNumber;
+
+                do
+                {
+                    newNumber++;
+                }
+                while (_oneCount != GetNumberOfOnes(newNumber));
+
+                CurrentNumber = newNumber;                
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private static int GetNumberOfOnes(uint number)
+        {
+            var count = 0;
+
+            while (number != 0)
+            {
+                if ((number & 1) == 1)
+                {
+                    count++;
+                }
+
+                number >>= 1;
+            }
+
+            return count;
+        }
+
+        private static uint GetFirstNumberWithEquelOnes(uint number, out int mostSignificantBitIndex)
+        {
+            uint firstNumber = 0;
+            mostSignificantBitIndex = 0;
+
+            for (uint i = number; i > 0; i >>= 1)
+            {
+                if ((i & 1) == 1)
+                {
+                    firstNumber <<= 1;
+                    firstNumber++;
+
+                    mostSignificantBitIndex++;
+                }
+            }
+
+            return firstNumber;
+        }
+    }
+}
